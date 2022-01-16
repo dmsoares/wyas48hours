5eef56b40459cf6ac7d3514a4ef454d7c5dee717
Decio Soares <decio.msoares@gmail.com>
2022-01-16 00:00:38 +0000

    Add Return Values example code.

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,6 @@
 module Main where
 
+import           Control.Monad
 import           System.Environment
 import           Text.ParserCombinators.Parsec hiding (spaces)
 
@@ -12,9 +13,40 @@ symbol :: Parser Char
 symbol = oneOf "!$%&|*+-/:<=?>@^_~#"
 
 readExpr :: String -> String
-readExpr input = case parse (spaces >> symbol) "lisp" input of
+readExpr input = case parse parseExpr "lisp" input of
   Left err  -> "No match: " ++ show err
   Right val -> "Found value"
 
 spaces :: Parser ()
 spaces = skipMany1 space
+
+data LispVal
+  = Atom String
+  | List [LispVal]
+  | DottedList [LispVal] LispVal
+  | Number Integer
+  | String String
+  | Bool Bool
+
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)
+  let atom = [first] ++ rest
+  return $ case atom of
+    "#t"      -> Bool True
+    "#f"      -> Bool False
+    otherwise -> Atom atom
+
+parseNumbers :: Parser LispVal
+parseNumbers = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom <|> parseString <|> parseNumbers
